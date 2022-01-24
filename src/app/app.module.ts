846de6b41242d39ab858535027ae5e2e62f21b26
Sergey <gitkhartmann.com>
2022-01-24 10:58:45 +0300

    изучение директив

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StyleDirective } from './directives/style.directive';
+import { IfNotDirective } from './directives/if-not.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		StyleDirective,
+  IfNotDirective
+	],
+	imports: [
+		BrowserModule,
+		FormsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
